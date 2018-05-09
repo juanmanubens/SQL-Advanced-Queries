13b25f10af2a9f2f2ca4534579af7a95bd9e98ef
Juan Manubens <manubens@wharton.upenn.edu>
2018-05-09 12:48:44 -0400

    Update hackerrank.sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -255,7 +255,8 @@ FROM STATION;
 
 
 # 16. Weather Observation Station 19 (Medium)
-
+SELECT ROUND(SQRT(POW(MAX(LAT_N) - MIN(LAT_N), 2) + POW(MAX(LONG_W) - MIN(LONG_W), 2)), 4) 
+FROM STATION;
 
 
 
